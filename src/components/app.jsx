c8c0e77edbbb9d82c1183d2533186ba1ddb458b2
Andrew Huth <ahuth@mavenlink.com>
2018-04-28 11:21:11 -0700

    Show two waves

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,10 @@ import ControlledSine from './controlled-sine';
 export default class App extends React.Component {
   render() {
     return (
-      <ControlledSine amplitude={5} frequency={30} phase={0} />
+      <div>
+        <ControlledSine amplitude={5} frequency={30} phase={0} />
+        <ControlledSine amplitude={5} frequency={30} phase={0} />
+      </div>
     );
   }
 }
